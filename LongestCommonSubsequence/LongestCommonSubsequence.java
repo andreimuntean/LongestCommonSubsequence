284d30c93cfd4613a507d56ed9f5daa062e436ea
andreimuntean <andrei.muntean.dev@gmail.com>
2015-03-07 20:34:27 +0000

    Finished it and added more tests

--- LongestCommonSubsequence/LongestCommonSubsequence.java
+++ LongestCommonSubsequence/LongestCommonSubsequence.java
@@ -1,25 +1,38 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
+/**
+ * Determines the longest common subsequence of two sequences.
+ *
+ * @author Andrei Muntean
+ */
 public class LongestCommonSubsequence
 {
     private static int[] firstSequence;
     private static int[] secondSequence;
-    private static int[][] dynamic;
 
-    private static void initialize()
+    // dp[i][j] = the size of the longest common subsequence up to the i-th element of
+    // firstSequence and the j-th element of secondSequence.
+    // The name "dp" derives from the concept used to calculate it: dynamic programming.
+    private static int[][] dp;
+
+    private static void read(String path) throws IOException
     {
-        try (Scanner scanner = new Scanner(System.in))
+        try (Scanner scanner = new Scanner(new FileReader(path)))
         {
-            firstSequence = new int[scanner.nextInt()];
+            // One-based indexing.
+            firstSequence = new int[scanner.nextInt() + 1];
+            secondSequence = new int[scanner.nextInt() + 1];
 
-            for (int index = 0; index < firstSequence.length; ++index)
+            for (int index = 1; index < firstSequence.length; ++index)
             {
                 firstSequence[index] = scanner.nextInt();
             }
 
-            secondSequence = new int[scanner.nextInt()];
-
-            for (int index = 0; index < secondSequence.length; ++index)
+            for (int index = 1; index < secondSequence.length; ++index)
             {
                 secondSequence[index] = scanner.nextInt();
             }
@@ -28,60 +41,64 @@ public class LongestCommonSubsequence
 
     private static void computeCommonSubsequenceLengths()
     {
-        boolean[] wasAssigned = new boolean[secondSequence.length];
+        dp = new int[firstSequence.length][secondSequence.length];
 
-        dynamic = new int[firstSequence.length][secondSequence.length];
-
-        for (int firstIndex = 0; firstIndex < firstSequence.length; ++firstIndex)
+        // i = the current index of the first sequence.
+        for (int i = 1; i < firstSequence.length; ++i)
         {
-            int firstElement = firstSequence[firstIndex];
-
-            for (int secondIndex = 0; secondIndex < secondSequence.length; ++secondIndex)
+            // j = the current index of the second sequence.
+            for (int j = 1; j < secondSequence.length; ++j)
             {
-                int secondElement = secondSequence[secondIndex];
-                int modifier = firstElement == secondElement ? 1 : 0;
-                int value = dynamic[firstIndex][secondIndex];
-
-                if (!wasAssigned[secondIndex] && firstElement == secondElement)
+                if (firstSequence[i] == secondSequence[j])
                 {
-                    value += 1;
-                    wasAssigned[secondIndex] = true;
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 }
-
-                if (firstIndex > 0)
+                else
                 {
-                    value += dynamic[firstIndex - 1][secondIndex];
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
-
-                if (secondIndex > 0)
-                {
-                    value = Math.max(value, dynamic[firstIndex][secondIndex - 1]);
-                }
-
-                dynamic[firstIndex][secondIndex] = value;
             }
         }
     }
 
-    private static int getLongestCommonSubsequenceLength()
+    // Magically (i.e. recursively) prints a common subsequence of maximum length.
+    private static void doMagic(PrintWriter printWriter, int i, int j)
     {
-        int result = 0;
-
-        for (int firstIndex = 0; firstIndex < dynamic.length; ++firstIndex)
+        if (0 < i && 0 < j)
         {
-            for (int secondIndex = 0; secondIndex < dynamic[firstIndex].length; ++secondIndex)
+            if (firstSequence[i] == secondSequence[j])
             {
-                result = Math.max(result, dynamic[firstIndex][secondIndex]);
+                doMagic(printWriter, i - 1, j - 1);
+                printWriter.printf("%d ", firstSequence[i]);
+            }
+            else if (dp[i][j - 1] >= dp[i - 1][j])
+            {
+                doMagic(printWriter, i, j - 1);
+            }
+            else
+            {
+                doMagic(printWriter, i - 1, j);
             }
         }
-
-        return result;
     }
 
-    public static void main(String[] args)
+    private static void write(String path) throws IOException
     {
-        initialize();
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(path)))
+        {
+            // Outputs the length of the longest common subsequence.
+            printWriter.println(dp[dp.length - 1][dp[0].length - 1]);
+
+            // Outputs the longest common subsequence.
+            doMagic(printWriter, dp.length - 1, dp[0].length - 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        // Opens the file "cmlsc.in" by default.
+        read(args != null && args.length > 0 ? args[0] : "cmlsc.in");
         computeCommonSubsequenceLengths();
-        System.out.println(getLongestCommonSubsequenceLength());
+        write(args != null && args.length > 1 ? args[1] : "cmlsc.out");
     }
 }
